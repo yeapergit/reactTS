7cce8e1c55736e4a5390dc32c72c7bdf5735aa2d
Pedro Ferreira <yeaper@gmail.com>
2024-03-04 11:28:55 +0000

    Polymorphic component with generics, forwaredRef

--- react-ts-advanced/src/App.tsx
+++ react-ts-advanced/src/App.tsx
@@ -1,13 +1,14 @@
+import { useRef } from "react";
 import Button from "./components/Button";
+import Container from "./components/Containet";
+import Input from "./components/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
+
   return <main>
-    <p>
-      <Button>a button</Button>
-    </p>
-    <p>
-      <Button href='https://google.com'>link</Button>
-    </p>
+    <Container as={Button} >Click Me</Container>
+    <Input label="test" id="test" ref={input} />
   </main>
 }
 
--- react-ts-advanced/src/components/Input.tsx
+++ react-ts-advanced/src/components/Input.tsx
@@ -1,13 +1,18 @@
-import React, { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<'input'>;
 
-export default function ({ label, id, ...props }: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { label, id, ...props },
+  ref
+) {
   return <p>
     <label htmlFor={id}>{label}</label>
-    <input id={id} {...props}></input>
+    <input id={id} {...props} ref={ref} />
   </p>
-}
+});
+
+export default Input;
